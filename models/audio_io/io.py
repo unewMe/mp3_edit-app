48edb24d2d62184a3a863d038199699d2d365eb9
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-13 21:35:49 +0200

    feat: add writing to given file type

--- models/audio_io/io.py
+++ models/audio_io/io.py
@@ -6,3 +6,10 @@ def read_audio_file(file_path: str) -> AudioFile:
     Read an audio file from the given file path.
     """
     return AudioFile.from_file(file_path)
+
+
+def write_audio_file(audio_file: AudioFile, file_path: str, file_format: str = "mp3") -> None:
+    """
+    Write an audio file to the given file path.
+    """
+    audio_file.export(file_path, format=file_format)
