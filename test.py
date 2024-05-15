de55281e280baff861fd4365a3dc35df891bc4f4
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-15 23:49:57 +0200

    feat: add AudioQueuePlayer tests

---
+++ test.py
@@ -0,0 +1,33 @@
+from models.mp3_players.AudioQueuePlayer import AudioQueuePlayer
+from models.audio_io.io import read_audio_file
+import time
+
+"""TESTING """
+
+def player_test():
+    player = AudioQueuePlayer()
+    player2 = AudioQueuePlayer()
+    player2.load("sound2", read_audio_file("Cypis.mp3"))
+    player.load("sound1", read_audio_file("Beethoven.mp3"))
+    player.play()
+    player2.play()
+    time.sleep(15)
+    player.pause()
+    player2.pause()
+    time.sleep(5)
+    player.resume()
+    player2.resume()
+    time.sleep(50)
+    player.stop()
+    player2.stop()
+
+def single_player_test():
+    player = AudioQueuePlayer()
+    player.load("sound1", read_audio_file("10.mp3"))
+    player.load("sound2", read_audio_file("10.mp3"), delay=5)
+    player.combine_audio_files()
+    player.play()
+    time.sleep(50)
+
+if __name__ == "__main__":
+    single_player_test()
