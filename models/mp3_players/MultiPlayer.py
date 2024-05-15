e4328e0ee702d5359748a7eede37e1b0b7530d7a
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-15 23:43:00 +0200

    feat: multiplayer template

---
+++ models/mp3_players/MultiPlayer.py
@@ -0,0 +1,29 @@
+from models.mp3_players.Player import Player
+
+class MultiPlayer:
+    def __init__(self, players_num=3):
+        self.players = [Player() for _ in range(players_num)]
+
+    def load_in_player(self, player_index, sound_id, file_path):
+        if 0 <= player_index < len(self.players):
+            self.players[player_index].load(sound_id, file_path)
+
+    def add_to_player_schedule(self, player_index, sound_id, delay):
+        if 0 <= player_index < len(self.players):
+            self.players[player_index].add_to_schedule(sound_id, delay)
+
+    def play_all(self):
+        for player in self.players:
+            player.play()
+
+    def pause_all(self):
+        for player in self.players:
+            player.pause()
+
+    def resume_all(self):
+        for player in self.players:
+            player.resume()
+
+    def stop_all(self):
+        for player in self.players:
+            player.stop()
