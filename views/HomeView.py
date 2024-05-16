4669a58b0a423193b8965bb798132080abe0fcd0
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-16 22:59:10 +0200

    feat: add QFrame next to bookmarks

--- views/HomeView.py
+++ views/HomeView.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget
+from PySide6.QtWidgets import QMainWindow, QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton, QListWidget, QFrame
 from PySide6.QtCore import Qt
 
 class HomeView(QMainWindow):
@@ -26,12 +26,6 @@ class HomeView(QMainWindow):
         self.tabs.addTab(self.files_tab, "Files")
         self.tabs.addTab(self.players_tab, "Players")
 
-        top_layout.addWidget(self.tabs)
-        top_layout.addStretch()
-
-        main_layout.addLayout(top_layout)
-        main_layout.addStretch(1)
-
         self.tabs.setFixedSize(800, 600)
 
         files_layout = QVBoxLayout(self.files_tab)
@@ -45,9 +39,20 @@ class HomeView(QMainWindow):
         files_buttons_layout.addWidget(delete_button)
         files_buttons_layout.addStretch(1)
 
-
         files_layout.addLayout(files_buttons_layout)
 
         self.files_list = QListWidget()
         self.files_list.setFixedSize(780, 540)
         files_layout.addWidget(self.files_list)
+
+        top_layout.addWidget(self.tabs)
+        top_layout.addStretch(1)
+
+        self.side_frame = QFrame()
+        self.side_frame.setFrameStyle(QFrame.Box | QFrame.Raised)
+        self.side_frame.setFixedSize(1800,600)
+        top_layout.addWidget(self.side_frame)
+        top_layout.setStretchFactor(self.side_frame, 1)
+
+        main_layout.addLayout(top_layout)
+        main_layout.addStretch(1)
