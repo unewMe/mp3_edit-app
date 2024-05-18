7416fa3aab73e178018dbbb009d17b263e0a1839
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-18 23:42:14 +0200

    feat: changed the class to abstract to improve inheritance flexibility

--- models/mp3_players/Player.py
+++ models/mp3_players/Player.py
@@ -2,86 +2,70 @@ import time
 from models.timers.Timer import Timer
 import pygame
 from models.audio_edit.AudioFile import AudioFile
+import abc
 
 
-class Player:
-    timer: Timer  # Timer object to keep track of the time of the sound file
+class Player(metaclass=abc.ABCMeta):
     play_obj: pygame.mixer.Sound | None  # Play object which represents the sound file
     channel: pygame.mixer.Channel | None  # Channel , which is used to play the sound file
     is_playing: bool  # Flag to check if the sound file is playing
-    audio_segment: AudioFile | None  # Pydub audio segment for in-memory editing
+    audio_file: AudioFile | None  # Pydub audio segment for in-memory editing
 
     def __init__(self):
         pygame.mixer.init()
         self.play_obj = None
         self.channel = None
         self.is_playing = False
-        self.timer = Timer()
-        self.audio_segment = None
+        self.audio_file = None
 
+    @abc.abstractmethod
     def is_loaded(self):
         """
         Check if a sound file is loaded.
         """
-        return self.play_obj is not None
+        pass
 
+    @abc.abstractmethod
     def load(self, audio_file: AudioFile):
         """
         Load a sound file from the given file path.
         """
-        self.stop()
-        self.audio_segment = audio_file
-        self.play_obj = pygame.mixer.Sound(self.audio_segment.to_buffer())
+        pass
 
+    @abc.abstractmethod
     def play(self):
         """
         Play the loaded sound file.
         """
-        if self.is_loaded():
-            self.channel = self.play_obj.play()
-            self.is_playing = True
-            self.timer.start()
+        pass
 
+    @abc.abstractmethod
     def pause(self):
         """
         Pause the sound file.
         """
-        if self.is_loaded() and self.is_playing:
-            self.channel.pause()
-            self.is_playing = False
-            self.timer.pause()
+        pass
 
+    @abc.abstractmethod
     def resume(self):
         """
         Resume the sound file.
         """
-        if self.is_loaded() and not self.is_playing:
-            self.channel.unpause()
-            self.is_playing = True
-            self.timer.resume()
+        pass
 
+    @abc.abstractmethod
     def stop(self):
         """
         Stop the sound file.
         """
-        if self.is_loaded():
-            self.channel.stop()
-            self.is_playing = False
-            self.timer.restart()
+        pass
 
-
+    @abc.abstractmethod
     def set_volume(self, volume: float):
         """
         Set the volume of the sound file.
         """
-        if self.is_loaded():
-            self.channel.set_volume(volume)
-
-    def get_current_time(self):
-        """
-        Get the current time of the sound file.
-        """
-        return self.timer.get_time()
+        pass
 
     def wait_for_finish(self):
         """
@@ -90,3 +74,16 @@ class Player:
         if self.is_loaded() and self.is_playing:
             while self.channel.get_busy():
                 pygame.time.delay(100)
+
+    def get_total_time(self):
+
+        if self.audio_file:
+            total_seconds = self.audio_file.duration_seconds
+            hours = total_seconds // 3600
+            total_seconds %= 3600
+            minutes = total_seconds // 60
+            total_seconds %= 60
+            seconds = total_seconds % 60
+            return hours, minutes, seconds
+        else:
+            return (0, 0, 0)
