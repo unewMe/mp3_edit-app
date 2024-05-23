f5c6794f75d511d0a5ae167f98b20da6ce897fdf
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-24 01:19:53 +0200

    feat: removed audio loading into single-player, add export to file all players

--- models/mp3_players/MultiPlayer.py
+++ models/mp3_players/MultiPlayer.py
@@ -1,35 +1,66 @@
+from pydub import AudioSegment
+
+from models.audio_edit.AudioFile import AudioFile
 from models.mp3_players.Player import Player
+from models.timers.Timer import Timer
+
 
 class MultiPlayer:
     """
     Class to manage multiple audio players.
     """
 
-    players: list[Player] # List of Player objects
+    players: dict[str, Player]  # Dict of Player objects
+    is_playing: bool  # Flag to check if the sound file is playing
+    timer: Timer  # Timer object to manage the playback time
 
-    def __init__(self, players_num=3):
-        self.players = [Player() for _ in range(players_num)]
+    def __init__(self, players: dict[str, Player]):
+        self.players = players
+        self.is_playing = False
+        self.timer = Timer()
 
-    def load_in_player(self, player_index, sound_id, file_path):
-        if 0 <= player_index < len(self.players):
-            self.players[player_index].load(sound_id, file_path)
-
-    def add_to_player_schedule(self, player_index, sound_id, delay):
-        if 0 <= player_index < len(self.players):
-            self.players[player_index].add_to_schedule(sound_id, delay)
-
-    def play_all(self):
-        for player in self.players:
+    def play_all(self) -> None:
+        self.is_playing = True
+        for _, player in self.players.items():
             player.play()
+        self.timer.start()
 
-    def pause_all(self):
-        for player in self.players:
+    def pause_all(self) -> None:
+        for _, player in self.players.items():
             player.pause()
+        self.timer.pause()
 
-    def resume_all(self):
-        for player in self.players:
+    def resume_all(self) -> None:
+        for _, player in self.players.items():
             player.resume()
+        self.timer.resume()
 
-    def stop_all(self):
-        for player in self.players:
+    def stop_all(self) -> None:
+        self.is_playing = False
+        for _, player in self.players.items():
             player.stop()
+        self.timer.stop()
+
+    def export(self, file_path: str, format="mp3") -> None:
+        """Combines all audio files from all players into one and exports them to a single file."""
+
+        max_length = 0
+        for _, player in self.players.items():
+            if isinstance(player.final_audio, AudioSegment):
+                max_length = max(max_length, len(player.final_audio))
+            else:
+                raise ValueError("player.final_audio is not an instance of AudioSegment")
+
+        combined_audio = AudioSegment.silent(duration=max_length)
+
+        for _, player in self.players.items():
+            if isinstance(player.final_audio, AudioSegment):
+                combined_audio = combined_audio.overlay(player.final_audio)
+            else:
+                raise ValueError("player.final_audio is not an instance of AudioSegment")
+
+        combined_audio.export(file_path, format=format)
+
+    def get_time(self) -> tuple[int, int, int]:
+        hours, minutes, seconds = self.timer.get_time()
+        return int(hours), int(minutes), int(seconds)
