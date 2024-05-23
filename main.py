05a42cdeb5fd2802005a1f229ce75a98cb9ea9c7
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-23 16:52:41 +0200

    refactor: use new gui starting structure

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
+
 from PySide6.QtWidgets import QApplication
+
 from controllers.HomeController import HomeController
+from views.HomeView import HomeView
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     controller = HomeController()
-
     controller.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
