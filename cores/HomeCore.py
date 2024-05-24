545d36cb4be35d005588b8c4c3a9d3de8e7eb81e
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-24 23:21:20 +0200

    feat: add player queue handling

--- cores/HomeCore.py
+++ cores/HomeCore.py
@@ -34,12 +34,13 @@ class HomeCore:
         """Creates a new player and adds it to the player list."""
         player = AudioQueuePlayer()
         self.players[f"Player{self.player_id}"] = player
-        self.multiplayer.add_player(f"{self.player_id}", player)
+        self.multiplayer.add_player(f"Player{self.player_id}", player)
         self.player_id += 1
 
     def remove_player(self, player_name) -> None:
         """Removes the selected player from the player list."""
         self.players.pop(player_name)
+        self.multiplayer.players.pop(player_name)
 
     def add_file_to_player(self, file_name, player_name) -> bool:
         """Adds the selected file to the selected player."""
@@ -77,3 +78,15 @@ class HomeCore:
     def get_current_time(self) -> tuple[int, int, int]:
         """Returns the current playback time."""
         return self.multiplayer.get_time()
+
+    def check_if_any_audio_is_loaded_into_player(self) -> bool:
+        """Checks if any audio is loaded into the player."""
+        return any(player.sound_files for player in self.players.values())
+
+    def update_player_order(self, player_name, order) -> None:
+        """Updates the play order of the selected player."""
+        self.players[player_name].set_play_order(order)
+
+    def remove_from_player(self, file_name, player_name):
+        """Removes the selected file from the selected player."""
+        self.players[player_name].remove(file_name)
