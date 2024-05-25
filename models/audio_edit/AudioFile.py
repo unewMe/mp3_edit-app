8aedb14063035c5e32131375770011cb557be134
Dawid Chudzicki <chudzickid@gmail.com>
2024-05-25 16:08:32 +0200

    refactor: creating new AudioFile takes cognisance of equalizer

--- models/audio_edit/AudioFile.py
+++ models/audio_edit/AudioFile.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 import math
 import io
 from models.audio_edit.filters import FilterType
-from models.audio_edit.equalizer import Equalizer, BaseBand
+from models.audio_edit.equalizer import Equalizer
 
 
 class AudioFile(AudioSegment):
@@ -11,15 +11,18 @@ class AudioFile(AudioSegment):
     """
 
     delay: int  # Delay in seconds before playing the audio file
-    bands: list[BaseBand]  # List of equalizer bands to apply to the audio file
+    volume: float  # Volume of the audio file
+    filters: list[FilterType]  # List of filters to apply to the audio file
+    equalizer: Equalizer  # Equalizer object to adjust the audio file
 
-    def __init__(self, data=None, bands: list[BaseBand] = [], *args, **kwargs):
+    def __init__(self, data=None, delay=0, filters=None, volume: float = 100, equalizer=None, *args,
+                 **kwargs):
         super().__init__(data, *args, **kwargs)
-        self.delay = 0
-        self.bands = bands
-        if bands:
-            for band in self.bands:
-                band.segment = self
+        filters = filters or []
+        self.delay = delay
+        self.volume = volume
+        self.filters = filters
+        self.equalizer = Equalizer(self) if not equalizer else equalizer
 
     @classmethod
     def from_segment(cls, segment: AudioSegment):
@@ -27,15 +30,27 @@ class AudioFile(AudioSegment):
                    channels=segment.channels)
 
     @classmethod
+    def from_audiofile(cls, audiofile):
+        return cls(audiofile._data, frame_rate=audiofile.frame_rate, sample_width=audiofile.sample_width,
+                   channels=audiofile.channels, filters=audiofile.filters, delay=audiofile.delay,
+                   volume=audiofile.volume, equalizer=audiofile.equalizer)
+
+    @classmethod
     def from_file(cls, file_path: str):
         return cls.from_segment(AudioSegment.from_file(file_path))
+
+    @classmethod
+    def combine_with_audio_segment(cls, audiofile, audio_segment: AudioSegment):
+        return cls(audio_segment._data, frame_rate=audio_segment.frame_rate, sample_width=audio_segment.sample_width,
+                   channels=audio_segment.channels, delay=audiofile.delay,
+                   volume=audiofile.volume, filters=audiofile.filters, equalizer=audiofile.equalizer)
 
     def _spawn(self, data: bytes, overrides={}):
         """
         Spawns a new AudioFile object with the given data and overrides.
         """
-        return self.__class__(data, bands=self.bands, frame_rate=self.frame_rate, sample_width=self.sample_width,
-                              channels=self.channels)
+        return self.__class__(data, frame_rate=self.frame_rate, sample_width=self.sample_width,
+                              channels=self.channels, delay=self.delay, volume=self.volume, filters=self.filters, equalizer=self.equalizer, **overrides)
 
     def to_buffer(self, format: str = "mp3"):
         """
@@ -69,12 +84,29 @@ class AudioFile(AudioSegment):
         """
         Sets the volume of the audio file.
         """
-        return self.apply_gain(math.log10(volume) * 10)
+        current_gain = -40.0 * math.log10(100 / self.volume) if self.volume > 0 else -80
+
+        target_gain = -40.0 * math.log10(100 / volume) if volume > 0 else -80
+
+        gain_adjustment = target_gain - current_gain
+
+        adjusted_audio_segment = self.apply_gain(gain_adjustment)
+
+        self.volume = volume
+
+        return self.combine_with_audio_segment(self, adjusted_audio_segment)
 
     def apply_filter(self, filter_type: FilterType):
         """
         Applies the given filter to the audio file.
         :return: AudioFile
         """
-        filter_instance = filter_type.value(self)
-        return filter_instance.apply()
+        self.filters.append(filter_type)
+        return filter_type.create_filter(self)
+
+    def set_value_on_band(self, band, value: float):
+        """
+        Sets the value on the given band of the equalizer.
+        :return: AudioFile
+        """
+        return self.equalizer.change_band_gain(band, value)
